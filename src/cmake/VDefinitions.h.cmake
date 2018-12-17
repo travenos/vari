f157abaf9bf7e9fe00cbf998a794b7cbb34a899b
Alexey Barashkov <barasher@yandex.ru>
2018-12-17 17:14:52 +0300

    Setting vendor informaion to the application

---
+++ src/cmake/VDefinitions.h.cmake
@@ -0,0 +1,17 @@
+/**
+ * Project VARI
+ * @author Alexey Barashkov
+ */
+
+#ifndef _VDEFINITIONS_H
+#define _VDEFINITIONS_H
+
+#define ORGANIZATION_NAME   "${PROJECT_VENDOR_LONG}"
+#define ORG_WEBSITE         "${ORG_WEBSITE}"
+#define APPLICATION_NAME    "${APP_NAME}"
+#define APPLICATION_VERSION "${PROJECT_VERSION_LONG}"
+
+#define YEAR_STRING         "${CURRENT_YEAR}"
+#define COMPILATION_TIME    "${CURRENT_TIME}"
+
+#endif // _VDEFINITIONS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
-#include "VWindowMain.h"
+/**
+ * Project VARI
+ * @author Alexey Barashkov
+ */
+
 #include <QApplication>
+
+#include "VDefinitions.h"
+#include "VWindowMain.h"
 
 int main(int argc, char *argv[])
 {
-    //TODO set application information: name, version, vendor, website
-    //Set russion translation for all elements
+    QCoreApplication::setOrganizationName(QStringLiteral(ORGANIZATION_NAME));
+    QCoreApplication::setOrganizationDomain(QStringLiteral(ORG_WEBSITE));
+    QCoreApplication::setApplicationName(QStringLiteral(APPLICATION_NAME));
+    QCoreApplication::setApplicationVersion(QStringLiteral(APPLICATION_VERSION));
+    //Set russian translation for all elements
     QLocale::setDefault(QLocale::C);
     QApplication a(argc, argv);
     VWindowMain w;
