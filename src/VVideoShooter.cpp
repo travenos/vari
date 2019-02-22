8995be6b160a873c49b129c674eede3460b6802a
Alexey Barashkov <barasher@yandex.ru>
2019-02-22 16:30:59 +0300

    Trying to clean up temp directory with frames for video on every start

--- src/VVideoShooter.cpp
+++ src/VVideoShooter.cpp
@@ -50,6 +50,7 @@ inline void VVideoShooter::constructorBody()
 {
     m_isSaving.store(false);
     setSuffixFileName(DEFAULT_SUFFIX_FILE_NAME);
+    QDir(SLIDES_DIR_PATH).removeRecursively();
 }
 
 VVideoShooter::~VVideoShooter()
